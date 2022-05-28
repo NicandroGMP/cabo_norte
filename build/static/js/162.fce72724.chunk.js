0e89181f53e50fc8c88d70258a1bfec7e4f60a2f
Daniel Castillo <daniel_tekit@hotmail.com>
2022-05-27 20:42:59 -0500

    feat(CN-1): remove build folder

--- build/static/js/162.fce72724.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcabonorte=self.webpackChunkcabonorte||[]).push([[162],{59162:function(e,n,r){r.r(n);r(72791);var a=r(64554),c=r(80184);n.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h2",{children:"Sample page 1"}),(0,c.jsx)(a.Z,{sx:{my:2},children:"You can kick start your app"})]})}}}]);
-//# sourceMappingURL=162.fce72724.chunk.js.map
