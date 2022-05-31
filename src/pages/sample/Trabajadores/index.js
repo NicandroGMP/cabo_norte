b68e776088e7cf58cb829dfe836bf03dd42a2595
GAMAMTP <nicandrogama@gmail.com>
2022-05-31 02:05:07 -0500

    feat(CN-3): sidebar-changes

--- src/pages/sample/Trabajadores/index.js
+++ src/pages/sample/Trabajadores/index.js
@@ -4,8 +4,8 @@ import { Box } from "@mui/material";
 const Trabajadores = () => {
   return (
     <>
-      <h2>Home</h2>
-      <Box sx={{ my: 2 }}>You can kick start your app</Box>
+      <h2>Trabajadores</h2>
+      <Box sx={{ my: 2 }}>Module Trabajadores</Box>
     </>
   );
 };
