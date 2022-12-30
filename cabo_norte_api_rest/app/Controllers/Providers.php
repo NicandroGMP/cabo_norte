368a1dc69cd29ab7e5a77805e54a2e196b798e19
Daniel Castillo <daniel_tekit@hotmail.com>
2022-12-29 18:42:47 -0600

    feat: add manager data

--- cabo_norte_api_rest/app/Controllers/Providers.php
+++ cabo_norte_api_rest/app/Controllers/Providers.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Controllers\BaseController;
 use App\Libraries\Hash;
 use App\Libraries\StringMake;
@@ -9,82 +10,85 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 
 
-class Providers extends BaseController{
+class Providers extends BaseController
+{
     public function __construct()
     {
-		helper(["url", "form"]);
+        helper(["url", "form"]);
 
         $this->providers = new ProvidersModel();
     }
-    public function index(){
-        $user_date = $this->providers->findAll();
+    public function index($managerId)
+    {
+        $user_date = $this->providers->where('manager', $managerId)->get()->getResultArray();
         return $this->getResponse([
             'message' => 'Provider retrieved successfully',
             'providers' => $user_date
         ]);
     }
- 
+
     public function registerProvider()
     {
         $rules = [
             "name" => "required",
-            "service"=> "required",
-            "work_id" => "required"
+            "service" => "required",
+            "work_id" => "required",
+            "manager_id" => "required"
         ];
 
         $input = $this->getRequestInput($this->request);
 
         if (!$this->validateRequest($input, $rules)) {
             return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
-        }else{
-            $number_worker=StringMake::manager_number() ;
+        } else {
+            $number_worker = StringMake::manager_number();
             $data = [
                 "register_number" => $number_worker,
                 "name" => $input["name"],
                 "service" => $input["service"],
                 "work" => $input["work_id"],
-                "status" => "Habilitado",
-
+                "manager" => $input["manager_id"],
+                "status" => "Habilitado"
             ];
             $query = $this->providers->insert($data);
-            if (!$query){
+            if (!$query) {
                 return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
-            }else{
+            } else {
                 return $this->getResponse([
-                    "message" => "El Proveedor ". $input["name"]."Se Registro Correctamente"
+                    "message" => "El Proveedor " . $input["name"] . "Se Registro Correctamente"
                 ]);
             }
         }
-
     }
     public function updateProvider()
     {
         $rules = [
             "name" => "required",
-            "service"=> "required",
+            "service" => "required",
         ];
         $input = $this->getRequestInput($this->request);
 
         if (!$this->validateRequest($input, $rules)) {
             return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
-        }else{
+        } else {
             $id = $input["id"];
             $data = [
                 "name" => $input["name"],
                 "service" => $input["service"],
-		];
-        $update = $this->providers->update($id, $data);
-        if(!$update){
-            return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
-        }else{
-            return $this->getResponse([
-                "message" => "datos actualizados"
-            ]);
-        }
+            ];
+            $update = $this->providers->update($id, $data);
+            if (!$update) {
+                return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
+            } else {
+                return $this->getResponse([
+                    "message" => "datos actualizados"
+                ]);
+            }
         }
     }
 
-    public function getProviderSearchByRegisterNumber(){
+    public function getProviderSearchByRegisterNumber()
+    {
 
         $input = $this->getRequestInput($this->request);
         $string = $input["search"];
@@ -94,84 +98,86 @@ class Providers extends BaseController{
         $join->join("managers", "workers.manager = managers.id")->where("workers.register_number", $string);
         $inf_user =  $join->get()->getResultArray();
 
-        if(!$inf_user){
+        if (!$inf_user) {
             return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
-        }else{
-         
-        return $this->getResponse([
-            "worker" => $inf_user
-        ]);   
-        }
-  }
-  public function getProviderScanByRegisterNumber($number){
+        } else {
 
-    $string = $number;
-    $join = $this->workers->table("workers");
-    $join->select('workers.id as workers_id, workers.name, works.job,workers.lastname, workers.position, workers.register_number, workers.company, CONCAT(managers.name," ",managers.lastname) as manager,CONCAT(works.job," ",works.batch) as job');
-    $join->join("works", "workers.job = works.id");
-    $join->join("managers", "workers.manager = managers.id")->where("workers.register_number", $string);
-    $inf_user =  $join->get()->getResultArray();
-
-    if(!$inf_user){
-        return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
-    }else{
-     
-    return $this->getResponse([
-        "worker" => $inf_user
-    ]);   
-    }
-}
-//consultar datos de provedor que registrados en el subcondominio y esten habilitados
-public function searchServicesByWorkId($id){
-    $join = $this->providers->table("providers");
-    $join->select('providers.id, providers.name, providers.service, providers.work as work_id,  CONCAT(works.job," ",works.batch) as job, providers.status as status_provider');
-    $join->join("works", "providers.work = works.id")->where("providers.work", $id)->where("providers.status" , "Habilitado");
-    $services = $join->get()->getResultArray();
-
-    return $this->getResponse(["services" =>  $services]);
-}
-
-//consultar datos de provedor que solicita un cono 
-public function searchServicesById($id){
-    $join = $this->providers->table("providers");
-    $join->select('providers.id, providers.register_number ,providers.name, providers.service, providers.work as work_id,  CONCAT(works.job," ",works.batch) as job, providers.status as status_provider');
-    $join->join("works", "providers.work = works.id")->where("providers.id", $id)->where("providers.status" , "Habilitado");
-    $services = $join->get()->getResultArray();
-
-    return $this->getResponse(["provider" =>  $services]);
-}
-public function statusUpdate($id,$data){
-    if (empty($id) || empty($data) ){
-        return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
-    }else{
-        if ($data === "Habilitado"){
-            $dataUpdate = ["status" => "Deshabilitado"];
-            $update = $this->providers->update($id,$dataUpdate);
-            return $this->getResponse(["message" => "deshabilitado"]);   
-        }elseif($data === "Deshabilitado"){
-            $dataUpdate = ["status" => "Habilitado"];
-            $update = $this->providers->update($id,$dataUpdate);
-            return $this->getResponse(["message" => "habilitado"]);   
-        }
+            return $this->getResponse([
+                "worker" => $inf_user
+            ]);
         }
     }
-    public function deleteProvider() {
-    $rules = [
-        "id"=> "required",
-    ];
-    $input = $this->getRequestInput($this->request);
-    if (!$this->validateRequest($input, $rules)) {
-        return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
-    }else{
-        $id = $input["id"];
-        $delete = $this->providers->delete($id);
-        if (!$delete){
-            return $this->getResponse("Error al eliminar Proveedor", ResponseInterface::HTTP_BAD_REQUEST);
+    public function getProviderScanByRegisterNumber($number)
+    {
+
+        $string = $number;
+        $join = $this->workers->table("workers");
+        $join->select('workers.id as workers_id, workers.name, works.job,workers.lastname, workers.position, workers.register_number, workers.company, CONCAT(managers.name," ",managers.lastname) as manager,CONCAT(works.job," ",works.batch) as job');
+        $join->join("works", "workers.job = works.id");
+        $join->join("managers", "workers.manager = managers.id")->where("workers.register_number", $string);
+        $inf_user =  $join->get()->getResultArray();
+
+        if (!$inf_user) {
+            return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
+        } else {
+
+            return $this->getResponse([
+                "worker" => $inf_user
+            ]);
         }
-        return $this->getResponse(["message" => "El Proveedor se ha eliminado"]);
     }
+    //consultar datos de provedor que registrados en el subcondominio y esten habilitados
+    public function searchServicesByWorkId($id)
+    {
+        $join = $this->providers->table("providers");
+        $join->select('providers.id, providers.name, providers.service, providers.work as work_id, providers.manager as manager_id, CONCAT(works.job," ",works.batch) as job, providers.status as status_provider');
+        $join->join("works", "providers.work = works.id")->where("providers.work", $id)->where("providers.status", "Habilitado");
+        $services = $join->get()->getResultArray();
+
+        return $this->getResponse(["services" =>  $services]);
     }
 
+    //consultar datos de provedor que solicita un cono 
+    public function searchServicesById($id)
+    {
+        $join = $this->providers->table("providers");
+        $join->select('providers.id, providers.register_number ,providers.name, providers.service, providers.work as work_id, providers.manager as manager_id, CONCAT(works.job," ",works.batch) as job, providers.status as status_provider');
+        $join->join("works", "providers.work = works.id")->where("providers.id", $id)->where("providers.status", "Habilitado");
+        $services = $join->get()->getResultArray();
 
+        return $this->getResponse(["provider" =>  $services]);
+    }
+    public function statusUpdate($id, $data)
+    {
+        if (empty($id) || empty($data)) {
+            return $this->getResponse("", ResponseInterface::HTTP_BAD_REQUEST);
+        } else {
+            if ($data === "Habilitado") {
+                $dataUpdate = ["status" => "Deshabilitado"];
+                $update = $this->providers->update($id, $dataUpdate);
+                return $this->getResponse(["message" => "deshabilitado"]);
+            } elseif ($data === "Deshabilitado") {
+                $dataUpdate = ["status" => "Habilitado"];
+                $update = $this->providers->update($id, $dataUpdate);
+                return $this->getResponse(["message" => "habilitado"]);
+            }
+        }
+    }
+    public function deleteProvider()
+    {
+        $rules = [
+            "id" => "required",
+        ];
+        $input = $this->getRequestInput($this->request);
+        if (!$this->validateRequest($input, $rules)) {
+            return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
+        } else {
+            $id = $input["id"];
+            $delete = $this->providers->delete($id);
+            if (!$delete) {
+                return $this->getResponse("Error al eliminar Proveedor", ResponseInterface::HTTP_BAD_REQUEST);
+            }
+            return $this->getResponse(["message" => "El Proveedor se ha eliminado"]);
+        }
+    }
 }
-?>
